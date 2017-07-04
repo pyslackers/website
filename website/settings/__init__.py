21cac8c9cf21ffc31132f1f12a6b9a5711a773e4
Matt Rasband <matt.rasband@gmail.com>
2017-07-03 19:41:53 -0600

    Add pip caching, testing config

--- website/settings/__init__.py
+++ website/settings/__init__.py
@@ -3,7 +3,10 @@ import os as _os
 _env = _os.getenv('PY_ENV')
 if _env == 'development':
     from .development import *  # noqa
+elif _env == 'testing':
+    from .testing import *  # noqa
 elif _env == 'production':
     from .production import *  # noqa
 else:
-    raise SystemExit('PY_ENV must be set to one of development|production')
+    raise SystemExit('PY_ENV must be set to one of '
+                     'development|testing|production')
