f18b9c0daac924ca4cbad4c375a5322fe325d32e
Matt Rasband <matt.rasband@gmail.com>
2017-07-25 20:54:15 -0600

    Fixes #32: Creates a core app for shared commands and eventual functionality

--- website/settings/_base.py
+++ website/settings/_base.py
@@ -84,6 +84,7 @@ INSTALLED_APPS = [
 
     'django_celery_beat',
 
+    'core',
     'blog',
     'marketing',
 ]
--- website/settings/development.py
+++ website/settings/development.py
@@ -15,4 +15,34 @@ DATABASES = {
 
 DEBUG = True
 
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'standard': {
+            'format': "[PYSLACKERSWEB] [%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",  # noqa
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'verbose': {
+            'format': '%(process)-5d %(thread)d %(name)-50s %(levelname)-8s %(message)s',  # noqa
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'disabled': False,
+            'propagate': True,
+        }
+    },
+}
+
 SECRET_KEY = 'PYSLACKERS_DEVELOPMENT_SECRET_KEY'
