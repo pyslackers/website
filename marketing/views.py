d975ea402bfe858d77ebc9f0a227684132798e36
Matt Rasband <matt.rasband@gmail.com>
2017-08-01 01:09:56 -0600

    Closes #11: Limits the number of repos visible on the landing page

--- marketing/views.py
+++ marketing/views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.cache import cache
 from django.views.generic import FormView, TemplateView
+from django.utils import dateparse
 
 from .forms import SlackInviteForm
 from .tasks import send_slack_invite
@@ -11,11 +12,17 @@ class Index(FormView):
     form_class = SlackInviteForm
     success_url = '/'
 
+    def _github_repos(self):
+        repos = cache.get('github_projects', [])[:6]
+        for repo in repos:
+            repo['updated_at'] = dateparse.parse_datetime(repo['updated_at'])
+        return repos
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(
             slack_member_count=cache.get('slack_member_count', 0),
-            github_repos=cache.get('github_projects', None),
+            github_repos=self._github_repos(),
         )
         return context
 
