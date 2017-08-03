willa75/website-1

This is a basic change for Dockerfile and travis build support to assist with the CD requirements in issue #15. Added a minor fix in the travis.yml for allowed_failures to have the correct scalar value as per travis lint.

website/settings/_base.py
@@ -58,8 +58,8 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': int(os.getenv('POSTGRES_PORT', 5432)),
     }
 }
 

pyslackers/website

website/settings/_base.py
@@ -77,12 +77,12 @@ CELERYBEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.{}'.format(os.getenv('DATABASE_BACKEND', 'postgresql')),
         'NAME': os.getenv('DATABASE_NAME', 'postgres'),
         'USER': os.getenv('DATABASE_USER', 'postgres'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD', ''),
         'HOST': os.getenv('DATABASE_HOST', 'localhost'),
-        'PORT': int(os.getenv('DATABASE_PORT', '5432')),
+        'PORT': int(os.getenv('DATABASE_PORT', 5432)),
     }
 }
 