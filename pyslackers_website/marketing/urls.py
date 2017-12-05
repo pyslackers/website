606458f24e14d45bfc1fc86e2409512306cb6487
Matt Rasband <matt.rasband@gmail.com>
2017-12-04 20:05:21 -0700

    Convert marketing urls to new path function

--- pyslackers_website/marketing/urls.py
+++ pyslackers_website/marketing/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 
 from .views import Index, TermsOfService, version
 
@@ -6,7 +6,7 @@ from .views import Index, TermsOfService, version
 app_name = 'marketing'
 
 urlpatterns = [
-    url(r'^$', Index.as_view(), name='index'),
-    url(r'^terms/$', TermsOfService.as_view(), name='tos'),
-    url(r'^version/$', version, name='version')
+    path('', Index.as_view(), name='index'),
+    path('terms/', TermsOfService.as_view(), name='tos'),
+    path('version/', version, name='version')
 ]
