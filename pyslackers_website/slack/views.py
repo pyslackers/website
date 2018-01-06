489e9471a1bfdef98de21f294ec35638a84beaed
Kevin Harper <kevinharperg@gmail.com>
2018-01-05 20:26:19 -0700

    #49: Adds chart showing memberships over time for about pg (/slack/)

--- pyslackers_website/slack/views.py
+++ pyslackers_website/slack/views.py
@@ -1,6 +1,9 @@
 import logging
 from collections import Counter
 
+import json
+import calendar
+
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.views.generic import FormView
@@ -25,13 +28,17 @@ class SlackInvite(FormView):
             latest_membership = Membership.objects.latest()
             member_count = latest_membership.member_count
             tz_count_json = latest_membership.tz_count_json
+            membership_history_json = self.get_membership_history_json()
         except Membership.DoesNotExist:
             member_count = 0
             tz_count_json = {}
+            membership_history = []
+            membership_history_json = {}
 
         context.update(
             slack_member_count=member_count,
             slack_member_tz_count=Counter(tz_count_json).most_common(100),
+            slack_membership_history_json=membership_history_json,
         )
         return context
 
@@ -44,3 +51,33 @@ class SlackInvite(FormView):
             messages.success(request, 'Invite sent, see you in Slack!')
             return self.form_valid(form)
         return self.form_invalid(form)
+
+    def get_membership_history_json(self):
+        """ Build membership history JSON """
+        counts = []
+        xlabels = []
+        is_first_record = True
+
+        membership_history = Membership.objects.values_list('member_count', 'timestamp')
+        for member_count, timestamp in membership_history:
+            counts.append(member_count)
+            # force year to appear on first xaxis label (ie: Mar 17)
+            if is_first_record:
+                xlabel = timestamp.strftime('%b %y')
+                is_first_record = False
+            elif timestamp.strftime('%b%d') == 'Jan01':
+            # new year detected, add MMM and YY to xlabels
+                xlabel = timestamp.strftime('%b %y')
+            elif timestamp.strftime('%d') == '01':
+            # new month detected, add MMM to xlabels
+                xlabel = timestamp.strftime('%b')
+            else:
+                continue
+
+            xlabels.append(xlabel)
+        membership_history_json = {
+                                        'counts' : counts,
+                                        'xlabels' : xlabels,
+
+                                    }
+        return json.dumps(membership_history_json)
