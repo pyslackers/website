580b90ffe3cb80dba6c2bcf068d5df57c960a293
Matt Rasband <matt.rasband@gmail.com>
2018-01-07 21:55:07 -0700

    #138: Adds tracking of emails invited by the website

--- pyslackers_website/slack/tests/test_slack_tasks.py
+++ pyslackers_website/slack/tests/test_slack_tasks.py
@@ -4,13 +4,17 @@ from mock import Mock
 import pook
 import pytest
 
-from pyslackers_website.slack.models import Membership
+from pyslackers_website.slack.models import (
+    Invite,
+    Membership,
+)
 from pyslackers_website.slack.tasks import (
     send_slack_invite,
     capture_snapshot_of_user_count
 )
 
 
+@pytest.mark.django_db
 class TestSendSlackInvite:
     def test_defaults_channels(self, settings, monkeypatch):
         mock_slack_client = Mock()
@@ -19,12 +23,13 @@ class TestSendSlackInvite:
         monkeypatch.setattr('pyslackers_website.slack.tasks.SlackClient',
                             mock_slack_client)
 
-        send_slack_invite('foobar@example.com')
+        send_slack_invite('foobar1@example.com')
         mock_slack_client.assert_called_with(settings.SLACK_OAUTH_TOKEN)
         (mock_slack_client.return_value.invite
-            .assert_called_with('foobar@example.com',
+            .assert_called_with('foobar1@example.com',
                                 ['foo', 'bar'],
                                 resend=True))
+        assert Invite.objects.latest().email == 'foobar1@example.com'
 
     def test_override_channels(self, settings, monkeypatch):
         mock_slack_client = Mock()
@@ -32,13 +37,25 @@ class TestSendSlackInvite:
         settings.SLACK_OAUTH_TOKEN = str(uuid.uuid4())
         monkeypatch.setattr('pyslackers_website.slack.tasks.SlackClient',
                             mock_slack_client)
+        mock_slack_client.return_value.invite(return_value=True)
 
-        send_slack_invite('foobar@example.com',
+        send_slack_invite('foobar2@example.com',
                           channels=['baz'], resend=False)
         mock_slack_client.assert_called_with(settings.SLACK_OAUTH_TOKEN)
         (mock_slack_client.return_value.invite
-            .assert_called_with('foobar@example.com',
+            .assert_called_with('foobar2@example.com',
                                 ['baz'], resend=False))
+        assert Invite.objects.latest().email == 'foobar2@example.com'
+
+    @pook.post('https://slack.com/api/users.admin.invite',
+               response_json={
+                   'ok': False,
+                   'error': 'already_invited'
+               })
+    def test_does_not_save_unsent_invite(self):
+        send_slack_invite('foobar3@example.com', channels=['baz'], resend=False)
+        with pytest.raises(Invite.DoesNotExist):
+            Invite.objects.latest()
 
 
 class TestCaptureSnapshotOfUserCount:
