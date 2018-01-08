3dd1e2dd4cbc0489180c7f417e798f155adf539f
Matt Rasband <matt.rasband@gmail.com>
2018-01-07 18:23:17 -0700

    \#156: Add testing for github project cache task

--- config/settings/testing.py
+++ config/settings/testing.py
@@ -1,6 +1,16 @@
 from ._base import *  # noqa
 
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': os.getenv('REDIS_URL', 'redis://'),
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
 CELERY_ALWAYS_EAGER = True
 
 SECRET_KEY = 'PYSLACKERS_TESTING_SECRET_KEY'
