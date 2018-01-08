165033e83e1e150202778a720de98a15c9c55ebc
Matt Rasband <mrasband@users.noreply.github.com>
2018-01-07 19:28:16 -0700

    156 hide deprecated GitHub repos (#167)
    #156: Only allows non-archived github repos to be included in the project features on the main page, add tests, use separate redis for testing locally

--- config/settings/testing.py
+++ config/settings/testing.py
@@ -1,6 +1,8 @@
 from ._base import *  # noqa
 
 
+CACHES['default']['LOCATION'] = 'redis://localhost:6379/1'  # noqa
+
 CELERY_ALWAYS_EAGER = True
 
 SECRET_KEY = 'PYSLACKERS_TESTING_SECRET_KEY'
--- pyslackers_website/marketing/tests/test_marketing_views.py
+++ pyslackers_website/marketing/tests/test_marketing_views.py
@@ -1,7 +1,7 @@
 import os
 import json
 
-from .views import version
+from pyslackers_website.marketing.views import version
 
 
 class TestVersionView:
@@ -16,11 +16,11 @@ class TestVersionView:
         assert data['revision'] == ''
 
     def test_app_version(self, rf):
-        COMMIT_SHA = 'b0dca52c215f8384c94c4930ca9b9957d824c202'
-        os.environ['APP_GIT_REVISION'] = COMMIT_SHA
+        commit_sha = 'b0dca52c215f8384c94c4930ca9b9957d824c202'
+        os.environ['APP_GIT_REVISION'] = commit_sha
         request = rf.get('/version')
         response = version(request)
 
         assert response.status_code == 200
         data = json.loads(response.content)
-        assert data['revision'] == COMMIT_SHA
+        assert data['revision'] == commit_sha
