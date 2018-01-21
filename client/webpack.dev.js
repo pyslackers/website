c649355e48d3fb5316c2048d609fd33ebd9bb80a
Matt Rasband <matt.rasband@gmail.com>
2018-01-21 09:32:46 -0700

    Fixes issue with local development not allowing fonts to load due to cors

--- client/webpack.dev.js
+++ client/webpack.dev.js
@@ -6,7 +6,12 @@ const webpack = require('webpack');
 module.exports = merge(common, {
   devServer: {
     hot: true,
-    publicPath: '/static/dist/'
+    publicPath: '/static/dist/',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+    }
   },
   plugins: [
     new webpack.NamedModulesPlugin(),
