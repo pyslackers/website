6823012e284cf4948384453980aab7a82d77bd12
Matt Rasband <matt.rasband@gmail.com>
2018-01-24 20:27:53 -0700

    Attempt to just ignore the error for disallowed hosts, as django is doing what we want

--- config/settings/production.py
+++ config/settings/production.py
@@ -19,6 +19,9 @@ DEBUG = False
 RAVEN_CONFIG = {
     'dsn': f'https://94925f1b36294c9eb5e71aa8b7251cb8:{os.environ.get("RAVEN_PASSWORD", "")}@sentry.io/269271',  # noqa
     'release': raven.fetch_git_sha(str(BASE_DIR)),  # noqa
+    'ignore_exceptions': [
+        'django.security.DisallowedHost'
+    ]
 }
 
 SESSION_COOKIE_SECURE = True
