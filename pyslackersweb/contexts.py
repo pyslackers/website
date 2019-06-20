250fad38bb72933ca1ca341f1cd9a3b2753be603
Ovv <contact@ovv.wtf>
2019-06-20 12:30:14 +0200

    Fetch and cache slack channels

--- pyslackersweb/contexts.py
+++ pyslackersweb/contexts.py
@@ -2,6 +2,7 @@ import asyncio
 
 from aiohttp import ClientSession, web
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from slack.io.aiohttp import SlackAPI
 
 from . import tasks
 
@@ -12,14 +13,18 @@ async def background_jobs(app: web.Application) -> None:
     github_job = tasks.sync_github_repositories(app)
     scheduler.add_job(github_job, "interval", hours=1)
 
-    slack_job = tasks.sync_slack_timezones(app)
-    scheduler.add_job(slack_job, "interval", hours=1)
+    slack_users_job = tasks.sync_slack_users(app)
+    scheduler.add_job(slack_users_job, "interval", hours=1)
+
+    slack_channels_job = tasks.sync_slack_channels(app)
+    scheduler.add_job(slack_channels_job, "interval", hours=1)
 
     scheduler.start()
 
     loop = asyncio.get_running_loop()
     loop.create_task(github_job())
-    loop.create_task(slack_job())
+    loop.create_task(slack_users_job())
+    loop.create_task(slack_channels_job())
 
     yield
 
@@ -30,3 +35,8 @@ async def client_session(app: web.Application) -> None:
     async with ClientSession(raise_for_status=True) as session:
         app["client_session"] = session
         yield
+
+
+async def slack_client(app: web.Application) -> None:
+    app["slack_client"] = SlackAPI(token=app["slack_token"], session=app["client_session"])
+    yield
