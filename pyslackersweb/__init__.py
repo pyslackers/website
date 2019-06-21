fe3e69948883016c974f6342b22cebb456e76b11
Quentin Dawans <contact@ovv.wtf>
2019-06-21 21:22:13 +0200

    Move pyslackers website as subapp under /web (#343)

--- pyslackersweb/__init__.py
+++ pyslackersweb/__init__.py
@@ -1,20 +1,12 @@
 import logging
 import os
-from pathlib import Path
 
 import sentry_sdk
 from aiohttp import web
-from aiohttp_jinja2 import setup as jinja2_setup, request_processor
-from aiohttp_remotes import XForwardedRelaxed, ForwardedRelaxed
-from jinja2 import FileSystemLoader
-from jinja2.filters import FILTERS
 from sentry_sdk.integrations.aiohttp import AioHttpIntegration
 from sentry_sdk.integrations.logging import LoggingIntegration
 
-from .contexts import background_jobs, client_session, apscheduler, slack_client
-from .filters import formatted_number
-from .middleware import request_context_middleware
-from .views import routes  # , on_oauth2_login
+from . import website
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -33,37 +25,17 @@ sentry_sdk.init(
 )
 
 
+async def index(request: web.Request) -> None:
+    location = request.app["website_app"].router["index"].url_for()
+    raise web.HTTPFound(location=location)
+
+
 async def app_factory() -> web.Application:
-    package_root = Path(__file__).parent
+    app = web.Application()
+    app.router.add_get("/", index)
 
-    app = web.Application(
-        middlewares=[
-            ForwardedRelaxed().middleware,
-            XForwardedRelaxed().middleware,
-            request_context_middleware,
-        ]
-    )
-    app.update(  # pylint: disable=no-member
-        static_root_url="/static",
-        github_repositories=[],
-        slack_user_count=0,
-        slack_timezones={},
-        slack_token=os.getenv("SLACK_TOKEN", os.getenv("SLACK_OAUTH_TOKEN")),
-        slack_invite_token=os.getenv("SLACK_INVITE_TOKEN", os.getenv("SLACK_OAUTH_TOKEN")),
-    )
-
-    jinja2_setup(
-        app,
-        context_processors=[request_processor],
-        loader=FileSystemLoader([package_root / "templates"]),
-        filters={"formatted_number": formatted_number, **FILTERS},
-    )
-
-    # this ordering is important, before reordering be sure to check
-    # for dependencies.
-    app.cleanup_ctx.extend([apscheduler, client_session, slack_client, background_jobs])
-
-    app.add_routes(routes)
-    app.router.add_static(app["static_root_url"], package_root / "static")
+    website_app = website.app_factory()
+    app.add_subapp("/web", website_app)
+    app["website_app"] = website_app
 
     return app
